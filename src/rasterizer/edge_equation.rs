3cef4539d1c4d946c20a20cc9c5986f48d292c6d
zm <zhaomingnpu@gmail.com>
2022-07-16 23:55:14 +0800

    增加edge equation模式
    N/A
    
    Signed-off-by: zm <zhaomingnpu@gmail.com>

---
+++ src/rasterizer/edge_equation.rs
@@ -0,0 +1,5 @@
+use super::triangle::Triangle;
+
+pub fn draw_trangle_edge_equation(image: &mut Vec<u8>, width: i32, height: i32, triangle: &Triangle) {
+
+}
--- src/rasterizer/mod.rs
+++ src/rasterizer/mod.rs
@@ -1,2 +1,4 @@
 pub mod rasterizer;
 pub mod triangle;
+pub mod edge_walking;
+pub mod edge_equation;
